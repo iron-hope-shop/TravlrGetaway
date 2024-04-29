34fdcf976e525b55418dbeaac64a107f4e852b3f
iron-hope-shop <me@brad-jackson.com>
2024-04-28 21:10:18 -0500

    ugh

---
+++ app_server/controllers/about.js
@@ -0,0 +1,11 @@
+const fs = require('fs')
+
+const about = (req, res) => {
+    pageTitle = 'Travlr Getaways - About';
+    res.render('about', {title: pageTitle});
+
+};
+
+module.exports = {
+    about
+}
